756910c6117206ef08ecb898b0c91378b8baf537
Mohammad Abdulfattah <150511675+MohammadAbdulfattah@users.noreply.github.com>
2024-02-12 23:17:38 +0300

    Update SeasonController.php

--- app/Http/Controllers/Api/SeasonController.php
+++ app/Http/Controllers/Api/SeasonController.php
@@ -7,9 +7,7 @@ use App\Models\Season;
 use App\Http\Resources\SeasonResource;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SeasonsResource;
-use App\Models\Season;
 use Illuminate\Http\Request;
-use App\Http\Traits\GeneralTrait;
 use Carbon\Carbon;
 
 class SeasonController extends Controller
@@ -25,4 +23,6 @@ class SeasonController extends Controller
         } catch (\Throwable $th) {
             return $this->errorResponse("not Found", 404);
         }
+    }
 }
+    
